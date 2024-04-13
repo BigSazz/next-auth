1afec1789b64c828db498bca438b652d1d7894c6
Osaze Oviawe <ocubed88@gmail.com>
2024-04-13 16:14:26 +0100

    - error fix

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react";
 export const useCurrentUser = () => {
   const session = useSession();
 
-  if (!session || !session.data || !session.data?.user) return null;
+  if (!session || !session.data || !session.data?.user) return undefined;
 
   return session.data?.user;
 };
